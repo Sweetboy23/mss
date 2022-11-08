9bafeee4c46716ef687cc34c28b7882154067f5d
Simon Khromov <Simafor01@mail.ru>
2022-11-08 12:22:41 +0300

    load

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+const int N = 10;
+int main()
+{
+
+	int array[N];
+
+	for (int j = 0; j < N; j++)
+	{
+		cout << "Enter array [" << j << "] -> ";
+		cin >> array[j];
+	}
+	// вывод подсказки
+	cout << "\n\nThe entered array\n";
+	// цикл вывода элементов массива
+	for (int j = 0; j < N; j++)
+		cout << array[j] << '\t';
+	cout << endl;
+	return 0;
+}
